0df8febff940090a095df73fdd11c0e784a53d2a
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-02-03 22:14:52 -0300

    header created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import Api from './api/Api';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
 const App = () => {
   return <div>
-    <Api/>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   </div>;
 };
 
