ced1c790a237cf6a6d4ca9f49908a34cf730f4d5
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-02-05 17:29:35 -0300

    hook useform

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
 
 const LoginForm = () => {
 
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
+  const username = useForm();
+  const password = useForm();
+  console.log(password.value);
 
-    function handleSubmit(event) {
-        event.preventDefault();
-    
+  function handleSubmit(event) {
+    event.preventDefault();
+    if(username.validate() && password.validate()){
         fetch(`${URL}/jwt-auth/v1/token`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({
-            username,
-            password,
-          }),
+          body: JSON.stringify(),
         })
           .then((response) => {
             console.log(response);
@@ -28,13 +27,14 @@ const LoginForm = () => {
           .then((json) => {
             console.log(json);
           });
+        }
       }
 
   return <section>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username"/>
-        <Input label="Senha" type="password" name="password"/>
+        <Input label="Usuário" type="text" name="username" {...username}/>
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to='/login/criar'>Cadastro</Link>
