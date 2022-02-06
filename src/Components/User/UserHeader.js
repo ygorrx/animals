1c5ede97e4d2c2aeba6dfc471ee56387f78e800e
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-02-06 20:25:21 -0300

    User Header Changes

--- src/Components/User/UserHeader.js
+++ src/Components/User/UserHeader.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import UserHeaderNav from './UserHeaderNav';
+import styles from './UserHeader.module.css';
+import { useLocation } from 'react-router-dom';
 
 const UserHeader = () => {
+
+const [title, setTitle] = React.useState('');
+const location = useLocation();
+console.log(location);
+
+React.useEffect(() => {
+  const {pathname} = location;
+  switch(pathname){
+    case '/conta/postar':
+      setTitle('Poste sua foto');
+      break;
+    case '/conta/estatisticas':
+      setTitle('Estatísticas');
+      break;
+    default:
+      setTitle('Minha conta');
+  }
+  
+}, [location])
+
   return (
-  <header>
-      <h1  className='title'>Header do Usuario</h1>
+  <header className={styles.header}>
+      <h1  className='title'>{title}</h1>
       <UserHeaderNav/>
   </header>
  );
