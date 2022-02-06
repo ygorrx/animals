23debb9cfee49a86d476404f6ce21118e370d04c
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-02-05 21:57:04 -0300

    Styling Tela de Login

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -5,6 +5,9 @@ import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
 import { UserContext } from '../../UserContext';
+import Error from '../Helper/Error';
+import styles from './LoginForm.module.css'
+import stylesBtn from '../Forms/Button.module.css'
 
 const LoginForm = () => {
 
@@ -19,19 +22,27 @@ const LoginForm = () => {
     }
   }
 
-  return <section>
+  return (
+    <section className='animeLeft'>
       {/* <Api/> */}
 
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+      <h1 className='title'>Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username}/>
         <Input label="Senha" type="password" name="password" {...password} />
         {loading ? <Button disabled>Carregando...</Button> :
         <Button>Entrar</Button>}
-        {error && <p>{error}</p>}
+        <Error error ={error}/>
       </form>
-      <Link to='/login/criar'>Cadastro</Link>
-  </section>;
+      <Link className={styles.perdeu} to='/login/perdeu'>Perdeu a senha?</Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={stylesBtn.button} to='/login/criar'>Cadastro</Link>
+      </div>
+      
+    </section>
+  );
 };
 
 export default LoginForm;
