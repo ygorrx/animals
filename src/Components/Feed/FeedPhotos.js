3b476c0d4555c4af03602cdc7b34541110e32785
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-02-07 22:31:44 -0300

    Modal Photo

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,7 +6,7 @@ import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css'
 
-const FeedPhotos = () => {
+const FeedPhotos = ({setModalPhoto}) => {
 
 const {data, loading, error, request} = useFetch();
 
@@ -26,7 +26,7 @@ React.useEffect(() => {
     return (
     <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) =>  (
-            <FeedPhotoItems key={photo.id} photo={photo}/>
+            <FeedPhotoItems setModalPhoto={setModalPhoto} key={photo.id} photo={photo}/>
         ))}
        
     </ul>
