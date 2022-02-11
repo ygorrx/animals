bbee2b1251a87155571d3ce283589990424d951d
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-02-10 21:54:18 -0300

    Scroll Infinito

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,19 +6,21 @@ import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css'
 
-const FeedPhotos = ({setModalPhoto}) => {
+const FeedPhotos = ({user, page, setModalPhoto, setInfinite}) => {
 
 const {data, loading, error, request} = useFetch();
 
 React.useEffect(() => {
      async function fetchPhotos(){
-        const {url, options} = PHOTOS_GET({page: 1, total: 6, user: 0});
+        const total = 3;
+        const {url, options} = PHOTOS_GET({page, total, user}); 
         const {response, json} = await request(url, options);
-        console.log(json);
-        console.log(response);
+        if(response && response.ok && json.length < total) setInfinite(false);
+        console.log('Request: ', json);
+        
      }
     fetchPhotos();
-}, [request]);
+}, [request, user, page, setInfinite]);
 
     if(error) return <Error error={error}/>
     if(loading) return <Loading/>
--- src/Components/Helper/Image.js
+++ src/Components/Helper/Image.js
@@ -3,15 +3,18 @@ import styles from './Image.module.css'
 
 const Image = ({alt, ...props}) => {
 
+const [skeleton, setSkeleton] = React.useState(true)
+
 
 function handleLoading({target}){
+    setSkeleton(false);
     target.style.opacity = 1;
-    
+
 }
 
   return (
     <div className={styles.wrapper}>
-        <div className={styles.skeleton}></div>
+        {skeleton && <div className={styles.skeleton}></div>}
         <img onLoad={handleLoading} className={styles.img} alt='' src='' {...props}/>
     </div>
   )
